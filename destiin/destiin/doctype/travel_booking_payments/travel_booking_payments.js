b7032e6053e1246c4ed26b828b875378a10f9be5
Shilpa <shilpa@avohilabs.com>
2026-01-07 12:10:28 +0530

    Added Payment Doc

---
+++ destiin/destiin/doctype/travel_booking_payments/travel_booking_payments.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2026, shilpa@avohilabs.com and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Travel Booking Payments", {
+// 	refresh(frm) {
+
+// 	},
+// });
