71220309b4ee499737f22c381917220b9cb9759a
Prethive <prethive@avohilabs.com>
2026-01-29 11:30:51 +0530

    request_id bug fixes

--- destiin/destiin/doctype/request_booking_details/request_booking_details.py
+++ destiin/destiin/doctype/request_booking_details/request_booking_details.py
@@ -7,8 +7,8 @@ from frappe.model.document import Document
 
 class RequestBookingDetails(Document):
 	def before_insert(self):
-		# Generate request_booking_id as employee_id + check_in + check_out
-		if self.employee and self.check_in and self.check_out:
+		# Only generate request_booking_id if not already set by API
+		if not self.request_booking_id and self.employee and self.check_in and self.check_out:
 			check_in_str = str(self.check_in).replace("-", "")
 			check_out_str = str(self.check_out).replace("-", "")
 			self.request_booking_id = f"{self.employee}_{check_in_str}_{check_out_str}"
